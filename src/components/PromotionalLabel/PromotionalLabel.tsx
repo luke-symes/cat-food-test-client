f900e77e0088023ed0759b3b3fe9bef215cc18eb
Luke <101844513+luke-symes@users.noreply.github.com>
2025-01-25 13:18:50 +0000

    Replace position prop with classes

--- src/components/PromotionalLabel/PromotionalLabel.tsx
+++ src/components/PromotionalLabel/PromotionalLabel.tsx
@@ -2,13 +2,14 @@ import classNames from 'classnames';
 
 interface Props {
   label: string;
-  position?: 'top-right';
+  className: string;
 }
 
-export function PromotionalLabel({ label, position }: Props) {
-  const classes = classNames('bg-pink-300 rotate-6 w-fit h-fit p-1 uppercase', {
-    'absolute -top-2 -right-2': position === 'top-right',
-  });
+export function PromotionalLabel({ label, className }: Props) {
+  const classes = classNames(
+    className,
+    'bg-pink-300 md:rotate-6 -rotate-6 w-fit h-fit p-1 uppercase absolute',
+  );
 
   return <div className={classes}>{label}</div>;
 }
