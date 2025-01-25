3da2effc0f99fd1e11ae26dd2cf758bd902b4c84
Luke <101844513+luke-symes@users.noreply.github.com>
2025-01-25 17:19:10 +0000

    Test for negative promo label & buttons

--- src/components/DeliveryCard/DeliveryCard.test.tsx
+++ src/components/DeliveryCard/DeliveryCard.test.tsx
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest';
-import { render } from 'vitest-browser-react';
+import { render } from '@testing-library/react';
 import { DeliveryCard } from './DeliveryCard';
 import { DeliverySchemaType } from '../../schemas/delivery';
 
@@ -14,7 +14,29 @@ describe('DeliveryCard', () => {
 
   it('renders free gift label if eligible', async () => {
     const screen = render(<DeliveryCard deliveryData={defaultProps} />);
-    const freeGiftLabel = await screen.getByText('Free gift').element();
+    const freeGiftLabel = await screen.queryByText('Free gift');
     await expect(freeGiftLabel).toBeInTheDocument();
+  });
+
+  it('does not render free gift label if not eligible', async () => {
+    const screen = render(
+      <DeliveryCard deliveryData={{ ...defaultProps, freeGift: false }} />,
+    );
+    const freeGiftLabel = await screen.queryByText('Free gift');
+    await expect(freeGiftLabel).not.toBeInTheDocument();
+  });
+
+  it('renders details button', async () => {
+    const screen = render(<DeliveryCard deliveryData={defaultProps} />);
+    const button = await screen.queryByRole('button', { name: 'See details' });
+    expect(button).toBeInTheDocument();
+  });
+
+  it('renders edit link', async () => {
+    const screen = render(<DeliveryCard deliveryData={defaultProps} />);
+    const link = await screen.queryByRole('link', {
+      name: 'Edit delivery',
+    });
+    expect(link).toBeInTheDocument();
   });
 });
