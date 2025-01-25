a96b190b36b704afbf57f0cd7078b975844d10a8
Luke <101844513+luke-symes@users.noreply.github.com>
2025-01-25 10:30:59 +0000

    Write story for delivery card

---
+++ src/components/DeliveryCard.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { DeliveryCard } from './DeliveryCard';
+
+const meta: Meta<typeof DeliveryCard> = {
+  component: DeliveryCard,
+  decorators: [
+    (Story) => (
+      <div className="max-w-lg">
+        <Story />
+      </div>
+    ),
+  ],
+};
+export default meta;
+
+type Story = StoryObj<typeof DeliveryCard>;
+
+export const Default: Story = {
+  args: {
+    deliveryData: {
+      title: 'Your next delivery for Tom & Jerry',
+      message:
+        "Hey Sonya! In two days' time, we'll be charging you for your next order for Tom and Jerry's fresh food.",
+      freeGift: true,
+      totalPrice: 135,
+    },
+  },
+};
