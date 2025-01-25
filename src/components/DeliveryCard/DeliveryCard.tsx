6289b9f9fd479b70fdddd3add35b2fe373255393
Luke <101844513+luke-symes@users.noreply.github.com>
2025-01-25 13:19:12 +0000

    Increase card padding & align promo label on mobile

--- src/components/DeliveryCard/DeliveryCard.tsx
+++ src/components/DeliveryCard/DeliveryCard.tsx
@@ -15,12 +15,12 @@ export function DeliveryCard({ deliveryData }: Props) {
   }
 
   return (
-    <div className="border-[1px] border-neutral-300 flex h-[244px] min-w-[752px] relative">
+    <div className="border-[1px] border-neutral-300 flex h-[244px] md:w-[752px] relative">
       <div
         className="bg-cover bg-center flex-1"
         style={{ backgroundImage: `url(${cat})` }}
       />
-      <div className="flex-[1.5] flex flex-col p-4 justify-between">
+      <div className="flex-[1.5] flex flex-col p-6 justify-between">
         <div className="flex flex-col gap-4">
           <h2 className="text-emerald-500 font-semibold text-lg">{title}</h2>
           <div>{message}</div>
@@ -39,7 +39,12 @@ export function DeliveryCard({ deliveryData }: Props) {
         </div>
       </div>
 
-      {freeGift && <PromotionalLabel label="Free gift" position="top-right" />}
+      {freeGift && (
+        <PromotionalLabel
+          label="Free gift"
+          className="md:-top-2 md:-right-2 md:left-auto md:translate-x-0 -bottom-4 left-1/2 -translate-x-1/2"
+        />
+      )}
     </div>
   );
 }
