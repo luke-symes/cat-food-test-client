29b3d0ea4a6aec5f761846325d8b1f1cda5a8101
Luke <101844513+luke-symes@users.noreply.github.com>
2025-01-25 15:16:11 +0000

    Fetch data using hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { DeliveryCard } from './components/DeliveryCard';
-import { mockDelivery } from './mocks/delivery';
+import { useDelivery } from './lib/delivery/useDelivery';
 
 function App() {
+  const userId = '618f4ed6-1c5b-4993-a149-f64700bf31dd';
+
+  const { data, error } = useDelivery(userId);
+
+  if (error) console.error(error);
+
   return (
     <div className="w-screen h-screen md:p-24 p-8 bg-neutral-200">
-      <DeliveryCard deliveryData={mockDelivery} />
+      {data && <DeliveryCard deliveryData={data} />}
     </div>
   );
 }
