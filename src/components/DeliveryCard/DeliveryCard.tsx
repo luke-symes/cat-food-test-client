9f16d604237e11f382c218027f2a3e35a86ca57a
Luke <101844513+luke-symes@users.noreply.github.com>
2025-01-25 13:53:55 +0000

    Align & wrap buttons

--- src/components/DeliveryCard/DeliveryCard.tsx
+++ src/components/DeliveryCard/DeliveryCard.tsx
@@ -15,12 +15,12 @@ export function DeliveryCard({ deliveryData }: Props) {
   }
 
   return (
-    <div className="border-[1px] border-neutral-300 flex h-[244px] md:w-[752px] relative md:text-left text-center">
+    <div className="border-[1px] border-neutral-300 flex md:min-h-[244px] md:max-w-[752px] relative md:text-left text-center bg-white">
       <div
         className="bg-cover bg-center md:flex-1 md:rounded-none md:block rounded-full absolute -top-6 md:translate-x-0 left-1/2 -translate-x-1/2 md:w-auto md:h-auto w-12 h-12 md:static"
         style={{ backgroundImage: `url(${cat})` }}
       />
-      <div className="flex-[1.5] flex flex-col p-6 justify-between">
+      <div className="flex-[1.5] flex flex-col p-6 gap-6">
         <div className="flex flex-col gap-4">
           <h2 className="text-emerald-500 font-semibold text-lg">{title}</h2>
           <div>{message}</div>
@@ -29,7 +29,7 @@ export function DeliveryCard({ deliveryData }: Props) {
           </div>
         </div>
 
-        <div className="flex gap-4">
+        <div className="flex gap-4 md:justify-start justify-center flex-wrap">
           <Button label="See details" onClick={handleClick} />
           <Button
             as="a"
