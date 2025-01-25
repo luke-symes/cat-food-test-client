3f466624160ea213a63142bc231e9bd856b5c7a9
Luke <101844513+luke-symes@users.noreply.github.com>
2025-01-25 17:22:03 +0000

    Install & configure image optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), ViteImageOptimizer()],
   test: {
     setupFiles: ['./vitest.setup.ts'],
     environment: 'jsdom',
