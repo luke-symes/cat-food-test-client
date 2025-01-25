16b112121c8e26df5b0b27371bfe95d25906273b
Luke <101844513+luke-symes@users.noreply.github.com>
2025-01-25 12:20:50 +0000

    Apply pointer cursor

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -18,7 +18,7 @@ export function Button({
   const Element = as;
 
   const classes = classNames(
-    'inline-block rounded-sm w-44 p-2 uppercase text-center',
+    'inline-block rounded-sm w-44 p-2 uppercase text-center cursor-pointer',
     {
       'bg-emerald-500 text-white': variant === 'primary',
       'bg-transparent text-emerald-500 border-[1px] border-emerald-500':
